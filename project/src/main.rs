2297ea96f39079a2ec0f9449c77ae7247b6021fd
spockwang <spockwang@tencent.com>
2022-07-29 16:17:41 +0800

    fix

--- project/src/main.rs
+++ project/src/main.rs
@@ -6,6 +6,7 @@ use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Error;
 use std::mem;
+use std::marker::PhantomData;
 
 pub mod raw_linked_list;
 use raw_linked_list::List;
@@ -13,21 +14,19 @@ mod vec;
 
 struct A<'a>(&'a i32);
 
+/*
 impl<'a> Drop for A<'a> {
     fn drop(&mut self) {
         println!("dropping a: {}", self.0);
     }
 }
-
-fn f1() {
-    let (a, i, mut b);
-    i = 1;
-    a = A{ 0: &i };
-    b = std::vec::Vec::new();
-    b.push(a);
-    println!("{}", b.len());
-}
-
+*/
 fn main() {
-    f1();
+    let mut v;
+    let (i, a);
+    i = 1;
+    a = A(&i);
+    v = vec::Vec::new();
+        v.push(a);
+    println!("{}", v.len());
 }
