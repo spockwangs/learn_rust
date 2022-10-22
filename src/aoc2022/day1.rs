373e41cf0df60cbccad1a92872de5a36c5c9c098
spockwang <wbbtiger@gmail.com>
2022-10-22 19:21:12 +0800

    fix

--- src/aoc2022/day1.rs
+++ src/aoc2022/day1.rs
@@ -11,10 +11,10 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
+    // Part 1
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("can't open file");
     let mut count = 0;
-    let mut previous = 0;
     contents.lines().enumerate().try_fold(0, |acc, (index, value)| {
         value.parse::<i32>().map(|num| {
             if index > 0 && acc < num {
@@ -24,5 +24,23 @@ fn main() -> ExitCode {
         })
     }).expect("invalid number");
     println!("count={}", count);
+
+    // Part 2
+    count = 0;
+    contents.lines().enumerate().try_fold([0; 3], |window, (index, value)| {
+        value.parse::<i32>().map(|num| {
+            let new_window = [window[1], window[2], num];
+            if index >= 3 {
+                let old_sum: i32 = window.iter().sum();
+                let new_sum = new_window.iter().sum();
+                if old_sum < new_sum {
+                    count += 1;
+                }
+            }
+            new_window
+        })
+    }).expect("invalid number");
+    println!("count={}", count);
+                
     return ExitCode::SUCCESS;
 }
